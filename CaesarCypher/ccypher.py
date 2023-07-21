7a7d97881b742bf501da84ed74e2a54c6fd591b9
arnaranjo <109238487+arnaranjo@users.noreply.github.com>
2023-07-21 18:54:48 +0200

    Create a class

--- CaesarCypher/ccypher.py
+++ CaesarCypher/ccypher.py
@@ -1,40 +1,46 @@
 import os
 import logo
-
 os.system('cls' if os.name == 'nt' else 'clear')
 
+class CCypher:
+    def __init__ (self, dataFile, alphabet):
+        self.dataFile = "CaesarCypher/character.txt"
+        self.alphabet = []
+
+
+    def DefineAlphabet(self):
+        # Generate a list from the file content.
+        with open(file=self.dataFile, mode='r',encoding="utf-8") as data:
+            self.alphabet = data.read().rsplit()
+
+
+    def Encrypt(self, mainText, step):
+        texEncrypted = ""
+        for lt in mainText:
+            newIndex = self.alphabet.index(lt) + step
+            if lt in self.alphabet:
+                texEncrypted += self.alphabet[newIndex if newIndex < len(self.alphabet) else newIndex - len(self.alphabet)]
+            # If the new position is out of the range, it will set the new index from the beginning. 
+        print(f"The text encrypted is: {texEncrypted}")
+
+
+    def Decrypt(self, mainText, step):
+        textDecrypted = ""
+        for lt in mainText:
+            newIndex = self.alphabet.index(lt) - step
+            if lt in self.alphabet:
+                textDecrypted += self.alphabet[newIndex if newIndex >= 0 else len(self.alphabet) + newIndex]
+            # If the new position is out of the range, it will set the index from the end of the list because newIndex is negative. 
+        print(f"The text decrypted is: {textDecrypted}")
+
+
+#--------------------------------------------------------------#
+
 print(logo.logo)
+KEEPON = True
+newCypher = CCypher()
 
-CONTINUE = True
-DATA_FILE = "CaesarCypher/character.txt"
-ALPHABET = []
-
-# Generate a list from the file content.
-with open(file=DATA_FILE, mode='r',encoding="utf-8") as data:
-    ALPHABET = data.read().rsplit()
-
-
-def Encrypt(mainText, step):
-    texEncrypted = ""
-    for lt in mainText:
-        newIndex = ALPHABET.index(lt) + step
-        if lt in ALPHABET:
-            texEncrypted += ALPHABET[newIndex if newIndex < len(ALPHABET) else newIndex - len(ALPHABET)]
-        # If the new position is out of the range, it will set the new index from the beginning. 
-    print(f"The text encrypted is: {texEncrypted}")
-
-
-def Decrypt(mainText, step):
-    textDecrypted = ""
-    for lt in mainText:
-        newIndex = ALPHABET.index(lt) - step
-        if lt in ALPHABET:
-            textDecrypted += ALPHABET[newIndex if newIndex >= 0 else len(ALPHABET) + newIndex]
-        # If the new position is out of the range, it will set the index from the end of the list because newIndex is negative. 
-    print(f"The text decrypted is: {textDecrypted}")
-
-
-while CONTINUE:
+while KEEPON:
     choice = input("Type 'E' to encrypt or type 'D' to decrypt a text: ")
     while choice != 'E' and choice != 'D':
         choice = input("Please, type 'E' to encrypt or type 'D' to decrypt a text: ")
@@ -42,13 +48,13 @@ while CONTINUE:
     try:
         if choice == 'E':
             mainText = input("Type the text to encrypt: ")
-            step = int(input("Set the encryption number: ")) % len(ALPHABET)
-            Encrypt(mainText, step)
+            step = int(input("Set the encryption number: ")) % len(newCypher.alphabet)
+            newCypher.Encrypt(mainText, step)
 
         elif choice == 'D':
             mainText = input("Type the text git: ")
-            step = int(input("Set the encryption number: ")) % len(ALPHABET)
-            Decrypt(mainText, step)
+            step = int(input("Set the encryption number: ")) % len(newCypher.alphabet)
+            newCypher.Decrypt(mainText, step)
     except:
         print("Error with the character.")
     
@@ -56,5 +62,5 @@ while CONTINUE:
     while selection != 'yes' and selection != 'no':
         selection = input("Please, type 'yes' or 'no': ")
     if selection == 'no':
-        CONTINUE = False
+        KEEPON = False
         print("Goodbye!")
