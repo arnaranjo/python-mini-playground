cdeb508cc99b9ab753d1c329e93262c198a3d819
arnaranjo <109238487+arnaranjo@users.noreply.github.com>
2023-07-21 23:28:39 +0200

    Add main file

--- CaesarCypher/ccypher.py
+++ CaesarCypher/ccypher.py
@@ -1,9 +1,12 @@
-import os
-import logo
-os.system('cls' if os.name == 'nt' else 'clear')
+'''
+CLASS CCypher:
+    Performs encryption and decryption of a string according to the Caesar cipher
+    It uses an external source of characters and as input the number of steps and the string.
+    (https://en.wikipedia.org/wiki/Caesar_cipher)
+'''
 
 class CCypher:
-    def __init__ (self, dataFile, alphabet):
+    def __init__ (self):
         self.dataFile = "CaesarCypher/character.txt"
         self.alphabet = []
 
@@ -17,7 +20,7 @@ class CCypher:
     def Encrypt(self, mainText, step):
         texEncrypted = ""
         for lt in mainText:
-            newIndex = self.alphabet.index(lt) + step
+            newIndex = self.alphabet.index(lt) + (step % len(self.alphabet))
             if lt in self.alphabet:
                 texEncrypted += self.alphabet[newIndex if newIndex < len(self.alphabet) else newIndex - len(self.alphabet)]
             # If the new position is out of the range, it will set the new index from the beginning. 
@@ -27,40 +30,8 @@ class CCypher:
     def Decrypt(self, mainText, step):
         textDecrypted = ""
         for lt in mainText:
-            newIndex = self.alphabet.index(lt) - step
+            newIndex = self.alphabet.index(lt) - (step % len(self.alphabet))
             if lt in self.alphabet:
                 textDecrypted += self.alphabet[newIndex if newIndex >= 0 else len(self.alphabet) + newIndex]
             # If the new position is out of the range, it will set the index from the end of the list because newIndex is negative. 
         print(f"The text decrypted is: {textDecrypted}")
-
-
-#--------------------------------------------------------------#
-
-print(logo.logo)
-KEEPON = True
-newCypher = CCypher()
-
-while KEEPON:
-    choice = input("Type 'E' to encrypt or type 'D' to decrypt a text: ")
-    while choice != 'E' and choice != 'D':
-        choice = input("Please, type 'E' to encrypt or type 'D' to decrypt a text: ")
-    
-    try:
-        if choice == 'E':
-            mainText = input("Type the text to encrypt: ")
-            step = int(input("Set the encryption number: ")) % len(newCypher.alphabet)
-            newCypher.Encrypt(mainText, step)
-
-        elif choice == 'D':
-            mainText = input("Type the text git: ")
-            step = int(input("Set the encryption number: ")) % len(newCypher.alphabet)
-            newCypher.Decrypt(mainText, step)
-    except:
-        print("Error with the character.")
-    
-    selection = input("Type 'yes' if you want to use the app again. Otherwise type 'no': ")
-    while selection != 'yes' and selection != 'no':
-        selection = input("Please, type 'yes' or 'no': ")
-    if selection == 'no':
-        KEEPON = False
-        print("Goodbye!")
---
+++ CaesarCypher/main.py
@@ -0,0 +1,41 @@
+import logo
+import os
+from ccypher import CCypher
+
+def main():
+    
+    KEEPON = True
+    newCypher = CCypher()
+    newCypher.DefineAlphabet()
+
+    while KEEPON:
+        choice = input("Type 'E' to encrypt or type 'D' to decrypt a text: ")
+        while choice != 'E' and choice != 'D':
+            choice = input("Please, type 'E' to encrypt or type 'D' to decrypt a text: ")
+        
+        try:
+            if choice == 'E':
+                mainText = input("Type the text to encrypt: ")
+                step = int(input("Set the encryption number: "))
+                newCypher.Encrypt(mainText, step)
+
+            elif choice == 'D':
+                mainText = input("Type the text git: ")
+                step = int(input("Set the encryption number: "))
+                newCypher.Decrypt(mainText, step)
+        except:
+            print("Error with the character.")
+        
+        selection = input("Type 'yes' if you want to use the app again. Otherwise type 'no': ")
+        while selection != 'yes' and selection != 'no':
+            selection = input("Please, type 'yes' or 'no': ")
+        if selection == 'no':
+            KEEPON = False
+            print("Goodbye!")    
+
+#--------------------------------------------------------------#
+
+if __name__ == "__main__":
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(logo.logo)
+    main()
