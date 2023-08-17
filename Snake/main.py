3ce33a98186b0dbe81b994ecc0ac42f9e6ada5ee
Alex R. Naranjo <109238487+arnaranjo@users.noreply.github.com>
2023-08-17 21:06:03 +0200

    Add movement to the snake

--- Snake/main.py
+++ Snake/main.py
@@ -4,6 +4,12 @@ from snake import Snake
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+
+KEY_UP = "Up"
+KEY_LEFT = "Left"
+KEY_DOWN = "Down"
+KEY_RIGHT = "Right"
+
 GAME = True
 
 screen = Screen()
@@ -12,12 +18,18 @@ screen.bgcolor("black")
 screen.title("Snake Game!")
 
 screen.tracer(0)
+screen.listen()
 
 newSnake = Snake()
 
 while GAME:
-    screen.update()
+    screen.update()    
     time.sleep(0.1)
+
     newSnake.MoveSnake()
+    screen.onkeypress(newSnake.UpSnake, KEY_UP)
+    screen.onkeypress(newSnake.LeftSnake, KEY_LEFT)
+    screen.onkeypress(newSnake.DownSnake, KEY_DOWN)
+    screen.onkeypress(newSnake.RightSnake, KEY_RIGHT)
 
 screen.mainloop()
--- Snake/snake.py
+++ Snake/snake.py
@@ -1,22 +1,38 @@
 from turtle import Turtle
 
 INIT_POSITION = [(-20 ,0), (-41 ,0), (-62 ,0)]
+DISTANCE = 21
 
 class Snake():
     def __init__(self):
         
-        self.snakeBody : Turtle = []
+        self.snakeBody = []
         self.Position()
         self.MoveSnake()
 
     def Position(self):
-        for i in INIT_POSITION:
+        for ps in INIT_POSITION:
             bodyElement = Turtle("square")
             bodyElement.penup()
             bodyElement.color("white")
-            bodyElement.setpos(i)
+            bodyElement.setpos(ps)
             self.snakeBody.append(bodyElement)
     
     def MoveSnake(self):
-        for element in self.snakeBody:
-            element.forward(21)
+        for element in range(len(self.snakeBody)-1, 0, -1):
+            newX = self.snakeBody[element - 1].xcor()
+            newY = self.snakeBody[element - 1].ycor()
+            self.snakeBody[element].goto(newX, newY)
+        self.snakeBody[0].forward(DISTANCE)
+
+    def UpSnake(self):
+        self.snakeBody[0].setheading(90)
+
+    def LeftSnake(self):
+        self.snakeBody[0].setheading(180)
+
+    def DownSnake(self):
+        self.snakeBody[0].setheading(270)
+
+    def RightSnake(self):
+        self.snakeBody[0].setheading(0)
