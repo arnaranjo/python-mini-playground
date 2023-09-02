06f41a029ce6048671f3664094b2c3b1621caacc
Alex R. Naranjo <109238487+arnaranjo@users.noreply.github.com>
2023-09-02 21:24:57 +0200

    Add menu and icon, rename gui file

--- ISSTraker/iss.py
+++ ISSTraker/iss.py
@@ -11,18 +11,22 @@ class issTrakerTurtle():
         self.issPosition = issPosition
 
         self.screen = TurtleScreen(canvas)
+        self.screen.register_shape("ISSTraker/space-station.gif")
         self.screen.setworldcoordinates(
             llx = -180.0,
             lly = -90.0,
             urx = 180.0,
             ury = 90.0
         )
+        self.screen.tracer(0)
     
     def setISS(self):
         self.issTurtle = RawTurtle(self.screen)
-        self.issTurtle.shape("circle")
+        self.issTurtle.shape("ISSTraker/space-station.gif")
         self.issTurtle.penup()
         self.issTurtle.goto(self.issPosition)
+        self.screen.update()
         
     def UpdateISSLocation(self, issPosition):
         self.issTurtle.goto(issPosition)
+        self.screen.update()
--- ISSTraker/main.py
+++ ISSTraker/main.py
@@ -1,28 +1,32 @@
 '''
 ISS current location web:
     http://open-notify.org/Open-Notify-API/ISS-Location-Now/
+Icon:
+    https://www.flaticon.com/free-icon/space-station_3212670
 '''
 
-from gui import issTrakerTurtle
+from iss import issTrakerTurtle
 import tkinter as tk
 import requests
+from tkinter import messagebox
 from PIL import Image
 
 API_URL = "http://api.open-notify.org/iss-now.json"
 BACKGROUND_IMG = "ISSTraker/worldmap.png"
 WIDTH :int
 HEIGHT :int
-UPDATE_TIME = 5.0
+UPDATE_TIME = 5000
+FONT_FAMILY = "arial"
+FONT_SIZE = 11
+FONT_TYPE = "normal"
 
 def DetermineImgSize():
-
     global WIDTH
     global HEIGHT
     imgSize = Image.open(BACKGROUND_IMG)
     WIDTH, HEIGHT = imgSize.size
         
 def RequestLocation():
-
     reqLocation = requests.get(API_URL)
     reqData = reqLocation.json()
 
@@ -31,12 +35,39 @@ def RequestLocation():
 
     return (lon, lat)
 
+def Exit():
+    quit()
+
+def About():
+    messagebox.showinfo(
+        title = "About",
+        message = 
+'''Where is the ISS?\n
+Don't worry, NASA has lost our space ship more than we expected but thanks to this app We can now stay updated on its location easily!\n
+https://github.com/arnaranjo/python-mini-playground'''
+    )
+
 def main():
+    DetermineImgSize()
 
     newWindow = tk.Tk()
     newWindow.title("ISS Traker")
-    newWindow.config(width = WIDTH,height = HEIGHT)
-        
+    newWindow.resizable(False,False)
+    menuBar = tk.Menu()
+
+    newWindow.config(
+        width = WIDTH,
+        height = HEIGHT,
+        menu = menuBar,
+        padx = 20,
+        pady = 20
+    )
+
+    menuView = tk.Menu(menuBar, tearoff = "off")
+    menuBar.add_cascade(labe = "View", menu = menuView)
+    menuHelp = tk.Menu(menuBar, tearoff = "off")
+    menuBar.add_cascade(label = "Help", menu = menuHelp)
+
     newCanvas = tk.Canvas(width = WIDTH,height = HEIGHT)
     newCanvas.pack()
 
@@ -46,10 +77,31 @@ def main():
     newCanvas.create_image((0,0), image = backgronundIMG)
 
     newISS.setISS()
-    
+
+    menuView.add_command(
+        label = "Update ISS location",
+        command = lambda: newISS.UpdateISSLocation(RequestLocation()),
+        font = (FONT_FAMILY, FONT_SIZE, FONT_TYPE)
+    )
+    menuView.add_command(
+        label = "Exit",
+        command = Exit,
+        font = (FONT_FAMILY, FONT_SIZE, FONT_TYPE)
+    )
+    menuHelp.add_command(
+        label = "About...",
+        command = About
+    )
+
+    def UpdateLocation():
+        newISS.UpdateISSLocation(RequestLocation())
+
+        newWindow.after(
+            ms = UPDATE_TIME,
+            func = UpdateLocation
+        )  
+
     newWindow.mainloop() 
 
 if __name__ == "__main__":
-
-    DetermineImgSize()
     main()
